3600fa5292478ce4ca63a57b36f0be6c1e5156bf
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-01 00:42:28 +0530

    Node readline CLI

--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const {kafka} = require('./client');
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+    input : process.stdin,
+    output : process.stdout
+})
 
 async function init (){
     const producer = kafka.producer();
@@ -7,18 +13,25 @@ async function init (){
     await producer.connect();
     console.log('Producer Connected Successfully');
 
-    await producer.send({
-        topic: "user-location-updates",
-        messages : [
-            {
-                partition : 0,
-                key:"location updates",
-                value : JSON.stringify({name:"Alice" , Loca:"Mumbai"})
-            }
-        ]
-    });
-    producer.disconnect();
-    
+    rl.setPrompt('> ')
+    rl.prompt();
+
+    rl.on('line', async function(line) {
+        const [userName , location] = line.split(' ');
+        await producer.send({
+            topic: "user-location-updates",
+            messages : [
+                {
+                    partition : location.toLowerCase() === "north" ? 0 :1,
+                    key:"location updates",
+                    value : JSON.stringify({name:userName , location:location})
+                }
+            ]
+        });
+    }).on('close', async ()=>{
+        producer.disconnect();
+    })
+
 }
 
 init();
